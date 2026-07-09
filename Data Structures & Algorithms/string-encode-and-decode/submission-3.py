1aeadea1b5c8245087602e4f78a138de48a9174d
Joanne Mariam Salil <121888369+joannxx@users.noreply.github.com>
2026-07-09 22:28:06 +0530

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,46 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded = []
+        for word in strs:
+            n = len(word)
+            encoded.append(f"{len(word)}#{word}")
+        return "".join(encoded)
+
+       
+    
+        
+
+     
+#pointers
+    def decode(self, s: str) -> List[str]:
+        decoded = []
+        i = 0 
+
+        
+        while i < len(s):
+            
+            #finding position #
+            j = i
+
+            while s[j] != '#':
+                j += 1
+
+            #Length of str 
+            l = int(s[i:j])
+
+            #extracting word
+            word = s[j+1:j+1+l]
+            decoded.append(word)
+
+            #Move to the next encoded word
+            i = j+1+l
+        
+        return decoded
+
+        
+
+            
+
+
+        
